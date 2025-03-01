efc31443e35f964fc792ae39cf74b46295e28a95
Christopher Setzer <37311998+cmsetzer@users.noreply.github.com>
2025-02-28 16:37:52 -0800

    Add initial WARCParser test

---
+++ tests/conftest.py
@@ -0,0 +1,25 @@
+import gzip
+from io import BufferedReader
+from pathlib import Path
+import zipfile
+
+import pytest
+
+
+@pytest.fixture
+def assets_path():
+    return Path(__file__).parent / 'assets'
+
+
+@pytest.fixture
+def wacz_file(assets_path: Path):
+    filepath = assets_path / 'example.com.wacz'
+    with filepath.open('rb') as doc:
+        yield doc
+
+
+@pytest.fixture
+def warc_file(wacz_file: BufferedReader):
+    with zipfile.Path(wacz_file, 'archive/data.warc.gz').open('rb') as warc_gz_file:
+        with gzip.open(warc_gz_file, 'rb') as warc_file:
+            yield warc_file
---
+++ tests/test_warc_parser.py
@@ -0,0 +1,13 @@
+import pytest
+
+from warcbench import WARCParser
+
+
+@pytest.mark.parametrize('parsing_style', ['delimiter', 'content_length'])
+def test_warc_parser_parse_by_delimiter(warc_file, parsing_style):
+    parser = WARCParser(warc_file, parsing_style=parsing_style)
+    parser.parse()
+
+    assert len(parser.records) == 9
+    assert parser.records[-1].start == 76091
+    assert parser.records[-1].end == 82943

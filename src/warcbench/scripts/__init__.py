07d24b5601c5ce490af77b8feb76a570601fbbc5
bensteinberg <bsteinberg@law.harvard.edu>
2025-03-05 10:02:08 -0500

    Add parse and extract subcommands

--- src/warcbench/scripts/__init__.py
+++ src/warcbench/scripts/__init__.py
@@ -1,18 +1,29 @@
 import click
-from warcbench.scripts.extra import goodbye
+import json
+import gzip
+import zipfile
+from mimetypes import guess_extension
+from pathlib import Path
+from warcbench import WARCParser
 from warcbench.scripts.example import parse_example
 
 
 @click.group()
+@click.option(
+    "-o",
+    "--out",
+    type=click.Choice(["raw", "json", "pprint"], case_sensitive=False),
+    default="raw",
+)
 @click.option("-v", "--verbose", count=True, help="Verbosity; repeatable")
 @click.pass_context
-def cli(ctx, verbose):
+def cli(ctx, out, verbose):
     """warcbench command framework, work in progress"""
     ctx.ensure_object(dict)
+    ctx.obj["OUT"] = out
     ctx.obj["VERBOSE"] = verbose
 
 
-cli.add_command(goodbye)
 cli.add_command(parse_example)
 
 
@@ -21,12 +32,148 @@ cli.add_command(parse_example)
 @click.pass_context
 def hello(ctx, world):
     """Hello!"""
-    if ctx.obj["VERBOSE"] > 2:
-        click.echo("There's a lot to say.")
-    click.echo(f"Hello {world}!")
+    msg = f"Hello {world}!"
+    if ctx.obj["OUT"] == "json":
+        click.echo(json.dumps({"message": msg}))
+    elif ctx.obj["OUT"] == "pprint":
+        click.echo(click.style(f"👋 {msg}", fg="green"))
+    else:
+        click.echo(msg)
 
 
 @cli.command()
-def extract():
-    """This could extract a file from a response record."""
-    raise click.ClickException("Not yet implemented")
+@click.argument("filepath", type=click.Path(exists=True, readable=True))
+@click.pass_context
+def parse(ctx, filepath):
+    """This counts the records found in the archive, and reports warning and error messages."""
+    ctx.obj["FILEPATH"] = filepath
+
+    parse_and_run(
+        ctx,
+        lambda p: click.echo(
+            f"Found {len(p.records)} records\nWarnings: {p.warnings}\nError: {p.error}"
+        ),
+    )
+
+
+@cli.command()
+@click.argument("filepath", type=click.Path(exists=True, readable=True))
+@click.argument("mimetype")
+@click.option(
+    "--basename", help="Base name for output file; defaults to FILEPATH base name."
+)
+@click.option(
+    "--idiom",
+    help="Choose your code path.",
+    type=click.Choice(["loop", "comprehension"], case_sensitive=False),
+    default="loop",
+)
+@click.pass_context
+def extract(ctx, filepath, mimetype, basename, idiom):
+    """This extracts files of the given MIMETYPE from the archive at FILEPATH, writing them to {basename}-{recordstart}.{extension}."""
+    ctx.obj["FILEPATH"] = filepath
+
+    if idiom == "loop":
+        f = _extract_loop
+    elif idiom == "comprehension":
+        f = _extract_comprehension
+
+    parse_and_run(
+        ctx,
+        partial_function(f, mimetype, basename if basename else Path(filepath).name),
+    )
+
+
+def _extract_loop(mimetype, basename, parser):
+    """A version of the file extractor using a loop."""
+    for r in parser.records:
+        try:
+            http_header_block = r.get_http_header_block().decode("utf-8")
+            if "content-type" in http_header_block.lower():
+                headers = http_header_block.split("\r\n")
+                for header in headers:
+                    if ": " in header:
+                        pair = header.split(": ")
+                        if (
+                            pair[0].lower() == "content-type"
+                            and pair[1].lower() == mimetype
+                        ):
+                            click.echo(
+                                f"Found a response of type {mimetype} at position {r.start}",
+                                err=True,
+                            )
+                            filename = (
+                                f"{basename}-{r.start}{guess_extension(mimetype)}"
+                            )
+                            Path(filename).parent.mkdir(exist_ok=True, parents=True)
+                            with open(filename, "wb") as f:
+                                f.write(r.get_http_body_block())
+        except AttributeError:
+            pass
+
+
+def _extract_comprehension(mimetype, basename, parser):
+    """A version of the file extractor using list comprehensions."""
+    for r in parser.records:
+        try:
+            if any(
+                [
+                    pair[1].lower() == mimetype
+                    for pair in [
+                        header.split(": ")
+                        for header in r.get_http_header_block()
+                        .decode("utf-8")
+                        .split("\r\n")
+                        if ": " in header
+                    ]
+                    if pair[0].lower() == "content-type"
+                ]
+            ):
+                click.echo(
+                    f"Found a response of type {mimetype} at position {r.start}",
+                    err=True,
+                )
+                filename = f"{basename}-{r.start}{guess_extension(mimetype)}"
+                Path(filename).parent.mkdir(exist_ok=True, parents=True)
+                with open(filename, "wb") as f:
+                    f.write(r.get_http_body_block())
+        except AttributeError:
+            pass
+
+
+def partial_function(func, *fixed_args):
+    """This closure provides partial application, for building the function passed to parse_and_run(). Note that the function passed in to this function should include `parser` as its *last* argument."""
+
+    def _partial_function(*args):
+        return func(*fixed_args, *args)
+
+    return _partial_function
+
+
+def parse_and_run(ctx, f):
+    """This function, not entirely DRY, handles three different archive file types, and runs the passed function on the parser."""
+    input_file = ctx.obj["FILEPATH"]
+    if input_file.lower().endswith(".wacz"):
+        with (
+            click.open_file(input_file, mode="rb") as wacz_file,
+            zipfile.Path(wacz_file, "archive/data.warc.gz").open("rb") as warc_gz_file,
+            gzip.open(warc_gz_file, "rb") as warc_file,
+        ):
+            parser = WARCParser(warc_file)
+            parser.parse()
+            f(parser)
+    elif input_file.lower().endswith(".warc.gz"):
+        with (
+            click.open_file(input_file, mode="rb") as warc_gz_file,
+            gzip.open(warc_gz_file, "rb") as warc_file,
+        ):
+            parser = WARCParser(warc_file)
+            parser.parse()
+            f(parser)
+    elif input_file.lower().endswith(".warc"):
+        with click.open_file(input_file, mode="rb") as warc_file:
+            parser = WARCParser(warc_file)
+            parser.parse()
+            f(parser)
+    else:
+        raise click.ClickException("This doesn't look like a web archive")
---
+++ tests/test_cli.py
@@ -0,0 +1,27 @@
+from click.testing import CliRunner
+from pathlib import Path
+from warcbench.scripts import cli
+
+
+def test_parse():
+    runner = CliRunner()
+    result = runner.invoke(cli, ["parse", "assets/example.com.wacz"])
+    assert result.exit_code == 0
+    assert "Found 9 records" in result.output
+
+
+def test_extract(tmp_path):
+    runner = CliRunner()
+    result = runner.invoke(
+        cli,
+        [
+            "extract",
+            "--basename",
+            f"{tmp_path}/example",
+            "assets/example.com.wacz",
+            "image/png",
+        ],
+    )
+    assert result.exit_code == 0
+    assert "Found a response of type image/png at position 5176" in result.output
+    assert Path(f"{tmp_path}/example-5176.png").exists()

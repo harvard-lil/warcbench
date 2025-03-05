97b80e3d0fca7e801c6653be2b15e3f0ab70e170
bensteinberg <bsteinberg@law.harvard.edu>
2025-03-05 16:10:04 -0500

    Handle msgtype without a preceding space

--- src/warcbench/filters.py
+++ src/warcbench/filters.py
@@ -113,7 +113,7 @@ def http_verb_filter(verb):
     """
 
     def f(record):
-        if record_content_type_filter("application/http; msgtype=request")(record):
+        if record_content_type_filter("msgtype=request")(record):
             http_headers = record.get_http_header_block()
             match = find_pattern_in_bytes(get_http_verb_pattern(verb), http_headers)
             if match:
@@ -131,7 +131,7 @@ def http_status_filter(status_code):
     """
 
     def f(record):
-        if record_content_type_filter("application/http; msgtype=response")(record):
+        if record_content_type_filter("msgtype=response")(record):
             http_headers = record.get_http_header_block()
             match = find_pattern_in_bytes(
                 get_http_status_pattern(status_code), http_headers
@@ -180,7 +180,7 @@ def http_response_content_type_filter(
     """
 
     def f(record):
-        if record_content_type_filter("application/http; msgtype=response")(record):
+        if record_content_type_filter("msgtype=response")(record):
             http_headers = record.get_http_header_block()
             match = find_pattern_in_bytes(
                 CONTENT_TYPE_PATTERN, http_headers, case_insensitive=case_insensitive

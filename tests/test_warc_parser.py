72defcb36f44bc5f4c5dd2d1712f78bf97641b61
Rebecca Cremona <rcremona@law.harvard.edu>
2025-03-28 13:59:04 -0400

    Test content lengths.

--- tests/test_warc_parser.py
+++ tests/test_warc_parser.py
@@ -19,3 +19,32 @@ def test_warc_parser_stop_after_nth(warc_file, parsing_style):
     parser = WARCParser(warc_file, parsing_style=parsing_style, stop_after_nth=2)
     parser.parse()
     assert len(parser.records) == 2
+
+
+def test_warc_parser_check_content_lengths_not_supported_in_content_length_mode(
+    warc_file,
+):
+    with pytest.raises(ValueError):
+        parser = WARCParser(
+            warc_file, check_content_lengths=True
+        )
+
+
+def test_warc_parser_check_content_lengths_false(warc_file):
+    # None, by default, when not checking
+    parser = WARCParser(
+        warc_file, parsing_style="delimiter", check_content_lengths=False
+    )
+    parser.parse()
+    for record in parser.records:
+        assert record.content_length_check_result is None
+
+
+def test_warc_parser_check_content_lengths_true(warc_file):
+    # True, for this valid WARC, when checking
+    parser = WARCParser(
+        warc_file, parsing_style="delimiter", check_content_lengths=True
+    )
+    parser.parse()
+    for record in parser.records:
+        assert record.content_length_check_result is True
